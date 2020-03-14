ae55bba709428d3f03e3c157b11456ddf6894d38
Liu Xiang <liuxiangxyd@163.com>
2020-03-14 12:35:24 +0800

    configure the saving directories

---
+++ conf.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+# 全局变量
+
+# define global variables, 
+# output files will be downloaded to following directories
+T_dir = r'F:\\cnwkw-cn\\ATTR\\' 
+L_dir = r'F:\\cnwkw-cn\\LD\\'
+S_dir = r'F:\\cnwkw-cn\\SD\\'
+H_dir = r'F:\\cnwkw-cn\\HD\\'
+M_dir = r'F:\\cnwkw-cn\\MP4\\'
+
+
